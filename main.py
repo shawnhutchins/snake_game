777dc930ac94a37fbf7fe9196f50d5a395d9e145
shawn <shawnhutchins1@gmail.com>
2025-08-06 21:12:41 -0400

    Created snake body

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snak!")
 
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
+snake_segments = []
 
+for position in starting_positions:
+    new_segment = Turtle(shape="square")
+    new_segment.color("white")
+    new_segment.goto(position)
+    snake_segments.append(new_segment)
 
 screen.exitonclick()
