56912db7a399d0294cb30a14827da21f8dd2b7b8
shawn <shawnhutchins1@gmail.com>
2025-08-11 21:12:55 -0400

    Moved snake code into snake class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 import time
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,24 +8,12 @@ screen.bgcolor("black")
 screen.title("Snak!")
 screen.tracer(0)
 
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-snake_segments = []
-
-for position in starting_positions:
-    new_segment = Turtle(shape="square")
-    new_segment.penup()
-    new_segment.color("white")
-    new_segment.goto(position)
-    snake_segments.append(new_segment)
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
-    for seg_num in range(len(snake_segments) - 1, 0, -1):
-        next_segment_pos = snake_segments[seg_num - 1].pos()
-        snake_segments[seg_num].goto(next_segment_pos)
-    snake_segments[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+
+class Snake:
+
+    def __init__(self):
+        self.snake_segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle(shape="square")
+            new_segment.penup()
+            new_segment.color("white")
+            new_segment.goto(position)
+            self.snake_segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.snake_segments) - 1, 0, -1):
+            next_segment_pos = self.snake_segments[seg_num - 1].pos()
+            self.snake_segments[seg_num].goto(next_segment_pos)
+        self.snake_segments[0].forward(MOVE_DISTANCE)
