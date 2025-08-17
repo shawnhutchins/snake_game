7f0bb6ab1d8c609b6023a8cf2b759ea0994818fa
shawn <shawnhutchins1@gmail.com>
2025-08-17 01:15:52 -0400

    Added detecting collisions with the snake tail

--- main.py
+++ main.py
@@ -38,4 +38,12 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
 
+    #Detect collision with tail
+    for segment in snake.snake_segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 screen.exitonclick()
